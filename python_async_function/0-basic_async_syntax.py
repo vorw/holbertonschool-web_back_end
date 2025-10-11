28cf7029bca0fb4f49d660cd34b16968e91b2900
vorw <sultanalsanie23@gmail.com>
2025-10-11 15:52:00 +0000

    Task

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Async coroutine waiting random delay and returning it."""
+"""Async coroutine that waits a random delay and returns it."""
 
 
 import asyncio
@@ -7,7 +7,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Wait random delay ≤ max_delay and return it."
+    """Wait for a random delay between 0 and max_delay and return it."""
     delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
