14faf27ba384d86dbf25ccc482e03a79de0c3730
vorw <sultanalsanie23@gmail.com>
2025-10-23 16:40:42 +0000

    Tasks

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -15,9 +15,10 @@ def index_range(page, page_size):
     end_index = page * page_size
     return (start_index, end_index)
 
+
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
