c3a5c587f590f4fce32673653c5d3aeccf15b74b
vorw <sultanalsanie23@gmail.com>
2025-10-29 22:17:01 +0300

    Task

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-from pymongo import MongoClient
+"""list all docs in a collection."""
+
+
+from typing import List, Dict, Any
+
+
 def list_all(mongo_collection):
     """list all docs in a collection"""
     return list(mongo_collection.find())
