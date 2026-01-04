94df12a397518a063a95eb41642a8df7141cd8c8
vorw <sultanalsanie23@gmail.com>
2026-01-05 01:43:18 +0300

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -8,7 +8,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   ]).then((results) =>
     results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
     }))
   );
 }
